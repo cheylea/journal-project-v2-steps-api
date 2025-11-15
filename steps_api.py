9222d7c79e41f59513637ce4b8bf3fed0d73c4e7
cheylea <cheyleahopkinson@gmail.com>
2025-11-15 01:21:26 +0000

    initial commit

---
+++ steps_api.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+import os
+from supabase import create_client, Client
+
+# Database connection
+SUPABASE_URL = os.environ.get("SUPABASE_URL") # railway
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY") # railway
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def add_steps(supabase, step_date, steps):
+        supabase.table("step").insert({
+            "stepdate": step_date,
+            "steps": steps
+        }).execute()
+
+app = FastAPI()
+
+class StepsPayload(BaseModel):
+    date: str
+    steps: int
+
+@app.post("/add_steps")
+def receive_steps(payload: StepsPayload):
+    jf.add_steps(supabase, payload.date, payload.steps)
+    return {"status": "ok", "message": "Steps recorded"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
